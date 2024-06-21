560f3ce166725e674d9c0caf2a254835c0dd82d1
Nimantha Perera <126785445+Nimantha-Perera@users.noreply.github.com>
2024-06-21 19:18:14 +0530

    Added popup notifications

--- lib/Notifications/notification_service.dart
+++ lib/Notifications/notification_service.dart
@@ -55,7 +55,7 @@ class NotificationService {
 
   void _initializeLocalNotifications() {
     const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
+        AndroidInitializationSettings('@drawable/notification');
     const InitializationSettings initializationSettings =
         InitializationSettings(android: initializationSettingsAndroid);
 
