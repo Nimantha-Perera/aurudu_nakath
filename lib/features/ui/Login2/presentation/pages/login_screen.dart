776cb8ed525b1fc431e710b0b9539daf72478ae9
Nimantha Perera <126785445+Nimantha-Perera@users.noreply.github.com>
2024-10-02 09:49:34 +0530

    error

--- lib/features/ui/Login2/presentation/pages/login_screen.dart
+++ lib/features/ui/Login2/presentation/pages/login_screen.dart
@@ -1,9 +1,11 @@
+// login_register_screen.dart
+import 'package:aurudu_nakath/features/ui/Login2/presentation/pages/widget/login_form.dart';
+import 'package:aurudu_nakath/features/ui/Login2/presentation/pages/widget/login_form.dart';
+import 'package:aurudu_nakath/features/ui/Login2/presentation/pages/widget/register_form.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'login_viewmodel.dart';
-import 'package:aurudu_nakath/features/ui/routes/routes.dart';
+
 
 class LoginRegisterScreen extends StatelessWidget {
   @override
@@ -14,13 +16,12 @@ class LoginRegisterScreen extends StatelessWidget {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          
-          backgroundColor: Colors.white,
+          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
           elevation: 0,
           bottom: TabBar(
-            labelColor: Colors.indigo.shade800,
+            labelColor: const Color.fromARGB(255, 255, 255, 255),
             unselectedLabelColor: Colors.grey,
-            indicatorColor: Colors.indigo.shade800,
+            indicatorColor: const Color.fromARGB(255, 255, 255, 255),
             tabs: [
               Tab(text: 'පූර්ණය වන්න'),
               Tab(text: 'නව ගිනුමක් සාදන්න'),
@@ -29,234 +30,11 @@ class LoginRegisterScreen extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            _buildLoginScreen(context, loginViewModel),
-            _buildRegisterScreen(context),
+            LoginForm(loginViewModel: loginViewModel),
+            RegisterForm(),
           ],
         ),
       ),
     );
-  }
-
-  Widget _buildLoginScreen(BuildContext context, LoginViewModel2 loginViewModel) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 50),
-          _buildLogo(),
-          SizedBox(height: 40),
-          _buildWelcomeText(),
-          SizedBox(height: 50),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'ඊමේල් ලිපිනය',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.email),
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'රහස්‍ය අංකය',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.lock),
-            ),
-          ),
-          SizedBox(height: 20),
-          Align(
-            alignment: Alignment.centerRight,
-            child: TextButton(
-              onPressed: () {
-                // Handle forgot password
-                showDialog(context: context, builder: (context) => AlertDialog(content: Text('Forgot Password functionality coming soon!')));
-              },
-              child: Text(
-                'Forgot Password?',
-                style: TextStyle(color: Colors.indigo.shade800),
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          _buildGoogleSignInButton(context, loginViewModel),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildRegisterScreen(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 30.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-      
-          SizedBox(height: 50),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Username',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.person),
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.email),
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Mobile Number',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.phone),
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.lock),
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Confirm Password',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              prefixIcon: Icon(Icons.lock),
-            ),
-          ),
-          SizedBox(height: 40),
-          SizedBox(
-  width: double.infinity,  // This will make the button's width full
-  child: ElevatedButton(
-    onPressed: () {
-      // Handle registration
-      showDialog(
-        context: context, 
-        builder: (context) => AlertDialog(content: Text('Registration functionality coming soon!'))
-      );
-    },
-    child: Text('Register', style: TextStyle(fontSize: 16)),
-    style: ElevatedButton.styleFrom(
-      backgroundColor: Colors.indigo.shade800,
-      foregroundColor: Colors.white,
-      padding: EdgeInsets.symmetric(vertical: 15),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30),
-      ),
-      elevation: 5,
-    ),
-  ),
-)
-
-        ],
-      ),
-    );
-  }
-
-  Widget _buildLogo() {
-    return Container(
-      width: 120,
-      height: 120,
-      decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage('assets/icons/katapatha.webp')),
-        color: Colors.white,
-        shape: BoxShape.circle,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 5,
-            blurRadius: 15,
-            offset: Offset(0, 5),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildWelcomeText() {
-    return Column(
-      children: [
-        Text(
-          'කැටපත',
-          style: GoogleFonts.notoSerifSinhala(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-            color: const Color.fromARGB(255, 90, 90, 90),
-          ),
-        ),
-        SizedBox(height: 10),
-        Center(
-          child: Text(
-            'ඔබගේ නිර්මාණශීලීත්වය අගය කිරීම සඳහා',
-            textAlign: TextAlign.center,
-            style: GoogleFonts.notoSerifSinhala(
-              fontSize: 15,
-              color: const Color.fromARGB(255, 90, 90, 90),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildGoogleSignInButton(BuildContext context, LoginViewModel2 loginViewModel) {
-    return ElevatedButton.icon(
-      icon: Icon(FontAwesomeIcons.google, color: Colors.indigo.shade800),
-      label: loginViewModel.isLoading
-          ? SizedBox(
-              width: 16,
-              height: 16,
-              child: CircularProgressIndicator(color: Colors.indigo.shade800, strokeWidth: 2),
-            )
-          : Text(
-              'Google සමගින් පූර්ණය වන්න',
-              style: TextStyle(fontSize: 16, color: Colors.indigo.shade800),
-            ),
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.indigo.shade800,
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        elevation: 5,
-      ),
-      onPressed: loginViewModel.isLoading ? null : () => _handleSignIn(context, loginViewModel),
-    );
-  }
-
-  void _handleSignIn(BuildContext context, LoginViewModel2 loginViewModel) async {
-    try {
-      await loginViewModel.login();
-      Navigator.pushReplacementNamed(context, AppRoutes.katapatha);
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Sign in failed. Please try again.')),
-      );
-    }
   }
 }
---
+++ lib/features/ui/Login2/presentation/pages/widget/register_form.dart
@@ -0,0 +1,308 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+class RegisterForm extends StatefulWidget {
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool _obscurePassword = true;
+  bool _obscureConfirmPassword = true;
+
+  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _mobileController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Dispose of the TextEditingControllers when the widget is removed from the widget tree
+    _usernameController.dispose();
+    _emailController.dispose();
+    _mobileController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            _buildWelcomeText(isDarkMode),
+            SizedBox(height: 40),
+            _buildTextField(
+                'පරිශීලක නාමය', Icons.person, _usernameController, isDarkMode),
+            SizedBox(height: 20),
+            _buildTextField(
+                'විද්‍යුත් තැපෑල', Icons.email, _emailController, isDarkMode,
+                keyboardType: TextInputType.emailAddress),
+            SizedBox(height: 20),
+            _buildTextField(
+                'ජංගම දුරකථන අංකය', Icons.phone, _mobileController, isDarkMode,
+                keyboardType: TextInputType.phone),
+            SizedBox(height: 20),
+            _buildPasswordField('මුරපදය', _passwordController, isDarkMode),
+            SizedBox(height: 20),
+            _buildPasswordField(
+                'මුරපදය තහවුරු කරන්න', _confirmPasswordController, isDarkMode,
+                isConfirmPassword: true),
+            SizedBox(height: 40),
+            _buildRegisterButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWelcomeText(bool isDarkMode) {
+    return Column(
+      children: [
+        SizedBox(height: 10),
+        Text(
+          'ඔබ යොදන රහස්‍ය අංකය කුමන හෝ ස්තානයක ලියා හෝ තබා ගන්න.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 13,
+            color: isDarkMode ? Colors.grey[300] : Colors.grey[600],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTextField(String label, IconData icon,
+      TextEditingController controller, bool isDarkMode,
+      {TextInputType? keyboardType}) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      decoration: _inputDecoration(label, icon, isDarkMode),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'කරුණාකර ඔබගේ $label ඇතුළත් කරන්න';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildPasswordField(
+      String label, TextEditingController controller, bool isDarkMode,
+      {bool isConfirmPassword = false}) {
+    return TextFormField(
+      controller: controller,
+      obscureText:
+          isConfirmPassword ? _obscureConfirmPassword : _obscurePassword,
+      decoration: _inputDecoration(label, Icons.lock, isDarkMode).copyWith(
+        suffixIcon: IconButton(
+          icon: Icon(
+            isConfirmPassword
+                ? (_obscureConfirmPassword
+                    ? Icons.visibility
+                    : Icons.visibility_off)
+                : (_obscurePassword ? Icons.visibility : Icons.visibility_off),
+            color: isDarkMode ? Colors.white70 : Colors.grey[600],
+          ),
+          onPressed: () {
+            setState(() {
+              if (isConfirmPassword) {
+                _obscureConfirmPassword = !_obscureConfirmPassword;
+              } else {
+                _obscurePassword = !_obscurePassword;
+              }
+            });
+          },
+        ),
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'කරුණාකර ඔබගේ $label ඇතුළත් කරන්න';
+        }
+        if (isConfirmPassword && value != _passwordController.text) {
+          return 'මුරපද නොගැලපේ';
+        }
+        return null;
+      },
+    );
+  }
+
+  InputDecoration _inputDecoration(
+      String label, IconData icon, bool isDarkMode) {
+    return InputDecoration(
+      labelText: label,
+      labelStyle:
+          TextStyle(color: isDarkMode ? Colors.white70 : Colors.grey[700]),
+      prefixIcon:
+          Icon(icon, color: isDarkMode ? Colors.white70 : Colors.grey[600]),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide.none,
+      ),
+      filled: true,
+      fillColor: isDarkMode ? Colors.grey[800] : Colors.grey[200],
+      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+    );
+  }
+
+  Widget _buildRegisterButton(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () => _handleRegister(context),
+        child: Text(
+          'ලියාපදිංචි වන්න',
+          style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 102, 102, 102)),
+        ),
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Theme.of(context).primaryColor,
+          padding: EdgeInsets.symmetric(vertical: 16),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          elevation: 3,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _handleRegister(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        // Check if username already exists in Firestore
+        final usernameQuery = await FirebaseFirestore.instance
+            .collection('normle_users')
+            .where('username', isEqualTo: _usernameController.text.trim())
+            .get();
+
+        if (usernameQuery.docs.isNotEmpty) {
+          _showErrorDialog(context, 'දෝෂයක් සිදුවී ඇත',
+              'පරිශීලක නාමය පෙරමුණුව ඇත.'); // Username already exists
+          return;
+        }
+
+        // Create the user with Firebase Authentication
+   
+          final UserCredential userCredential = await FirebaseAuth.instance
+              .createUserWithEmailAndPassword(
+                  email: _emailController.text.trim(),
+                  password: _passwordController.text.trim());
+          final User? user = userCredential.user;
+         
+
+        // Store user details in Firestore
+        await FirebaseFirestore.instance
+            .collection('normle_users')
+            .doc(user!.uid)
+            .set({
+          'username': _usernameController.text.trim(),
+          'email': _emailController.text.trim(),
+          'mobile': _mobileController.text.trim(),
+          'reg_date': DateTime.now().toString(),
+          'uid': user.uid,
+        });
+
+        // Save data to SharedPreferences
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('email', user.email ?? '');
+        await prefs.setString('displayName', _usernameController.text.trim());
+        await prefs.setString('userId', user.uid);
+
+        // Clear fields after successful registration
+        _usernameController.clear();
+        _emailController.clear();
+        _mobileController.clear();
+        _passwordController.clear();
+        _confirmPasswordController.clear();
+
+        // Show success message
+        _showErrorDialog(context, 'ලියාපදිංචිය සාර්ථකයි',
+            'ඔබ ලියාපදිංචි විය.'); // Registration successful
+      } on FirebaseAuthException catch (e) {
+        // Handle registration error
+        String errorMessage;
+
+        switch (e.code) {
+          case 'ERROR_EMAIL_ALREADY_IN_USE':
+            errorMessage =
+                'මෙම විද්‍යුත් තැපෑල සම්පූර්ණයෙන්ම පවත්වාගෙන යනු ලබයි. (This email address is already in use.)';
+            // Show alert for email already in use
+            _showAlertDialog(context, 'දෝෂයක් සිදුවී ඇත', errorMessage);
+            return; // Exit the method
+          case 'weak-password':
+            errorMessage = 'මුරපදය දුර්වලයි. (Weak password)';
+            break;
+          case 'ERROR_INVALID_EMAIL':
+            errorMessage =
+                'වලංගු විද්‍යුත් තැපෑලක් ඇතුළත් කරන්න. (Please enter a valid email address.)';
+            break;
+          default:
+            errorMessage =
+                'ලියාපදිංචි කිරීමේ දෝෂයක් සිදුවී ඇත. (Registration error)';
+            break;
+        }
+        _showErrorDialog(context, 'දෝෂයක් සිදුවී ඇත', errorMessage);
+      } catch (e) {
+        // Handle other errors
+        _showErrorDialog(context, 'දෝෂයක් සිදුවී ඇත',
+            'ලියාපදිංචි කිරීමේ දෝෂයක් සිදුවී ඇත. (Registration error)');
+      }
+    }
+  }
+
+// Function to show an alert dialog
+  void _showAlertDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text('හරි'), // "OK" in Sinhala
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showErrorDialog(BuildContext context, String title, String content) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('ඉවත් වන්න'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
